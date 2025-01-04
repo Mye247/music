4b4f2e8dd829295d5d33381a52e9d271a83592bb
Mye247 <louemc1@gmail.com>
2025-01-05 00:20:14 +0900

    feat: api작업, 적용하기

--- app/(providers)/(root)/_components/Header.tsx
+++ app/(providers)/(root)/_components/Header.tsx
@@ -1,9 +1,12 @@
 "use client";
 
+import unifiedAPI from "@/api/unifiedAPI";
 import { supabase } from "@/supabase/client";
 import { useAuthStore } from "@/zustand/authStore";
 import { useModalStore } from "@/zustand/modalStore";
+import { User } from "@supabase/supabase-js";
 import Link from "next/link";
+import { useEffect, useState } from "react";
 import LogInModal from "./Modals/LogInModal";
 
 function Header() {
@@ -11,6 +14,7 @@ function Header() {
   const openModal = useModalStore((state) => state.openModal);
   const isLoggedIn = useAuthStore((state) => state.isLoggedIn);
   const logOut = useAuthStore((state) => state.logOut);
+  const [loginUser, setLoginUser] = useState<User | null>();
 
   // 핸들러
 
@@ -25,6 +29,15 @@ function Header() {
     logOut();
   };
 
+  // 유저 정보 갱신
+  useEffect(() => {
+    const loginUserData = async () => {
+      const getUser = await unifiedAPI.getUserApi.getUser();
+      setLoginUser(getUser);
+    };
+    loginUserData();
+  }, []);
+
   return (
     <header className="min-w-full bg-cyan-100 min-h-[80px] flex items-center justify-between">
       <Link href={"/"}>
@@ -33,6 +46,7 @@ function Header() {
 
       {isLoggedIn ? (
         <div className="flex gap-x-3 pr-5">
+          <p>{loginUser?.user_metadata.display_name}</p>
           <button onClick={handleClickLogOutButton}>로그아웃</button>
         </div>
       ) : (
--- app/(providers)/(root)/page.tsx
+++ app/(providers)/(root)/page.tsx
@@ -1,5 +1,9 @@
 import Page from "./_components/Page";
 
 export default function HomePage() {
-  return <Page title="메인 페이지 입니다.">하이요</Page>;
+  return (
+    <Page title="메인 페이지 입니다.">
+      최신글과 인기글 등등이 들어갈 예정입니다.
+    </Page>
+  );
 }
