002538e3fa216953a4731c683e047aa3cdaa51d5
Mye247 <louemc1@gmail.com>
2025-01-07 01:49:46 +0900

    chore: 수정 버튼 추가

--- app/(providers)/(root)/community/post/new/page.tsx
+++ app/(providers)/(root)/community/post/new/page.tsx
@@ -4,6 +4,7 @@ import unifiedAPI from "@/api/unifiedAPI";
 import { useRouter } from "next/navigation";
 import { useState } from "react";
 import { toast } from "react-toastify";
+import Page from "../../../_components/Page";
 
 const AddNewPostPage = () => {
   const router = useRouter();
@@ -39,56 +40,58 @@ const AddNewPostPage = () => {
   };
 
   return (
-    <div className="max-w-lg mx-auto mt-10 p-6 bg-gray-900 text-gray-100 shadow-md rounded">
-      <h1 className="text-2xl font-bold mb-6 text-center text-gray-50">
-        글 작성하기
-      </h1>
-      <form onSubmit={(e) => e.preventDefault()}>
-        {/* 제목 */}
-        <label htmlFor="title" className="block text-sm font-medium mb-2">
-          제목
-        </label>
-        <input
-          type="text"
-          id="title"
-          className="w-full p-3 border border-gray-700 rounded bg-gray-800 text-gray-100 focus:outline-none focus:ring-2 focus:ring-blue-500 mb-4"
-          placeholder="제목을 입력하세요"
-          value={title}
-          onChange={(e) => setTitle(e.target.value)}
-        />
+    <Page title="new post">
+      <div className="max-w-[1000px] h-[600px]  p-6 bg-gray-900 text-gray-100 shadow-lg rounded">
+        <h1 className="text-2xl font-bold mb-6 text-center text-gray-50">
+          글 작성하기
+        </h1>
+        <form onSubmit={(e) => e.preventDefault()}>
+          {/* 제목 */}
+          <label htmlFor="title" className="block text-sm font-medium mb-2">
+            제목
+          </label>
+          <input
+            type="text"
+            id="title"
+            className="w-full p-3 border border-gray-700 rounded bg-gray-800 text-gray-100 focus:outline-none focus:ring-2 focus:ring-blue-500 mb-4"
+            placeholder="제목을 입력하세요"
+            value={title}
+            onChange={(e) => setTitle(e.target.value)}
+          />
 
-        {/* 본문 */}
-        <label htmlFor="content" className="block text-sm font-medium mb-2">
-          본문
-        </label>
-        <textarea
-          id="content"
-          className="w-full p-3 border border-gray-700 rounded bg-gray-800 text-gray-100 focus:outline-none focus:ring-2 focus:ring-blue-500 mb-4"
-          rows={5}
-          placeholder="내용을 입력하세요"
-          value={content}
-          onChange={(e) => setContent(e.target.value)}
-        />
+          {/* 본문 */}
+          <label htmlFor="content" className="block text-sm font-medium mb-2">
+            본문
+          </label>
+          <textarea
+            id="content"
+            className="w-full p-5 border border-gray-700 rounded bg-gray-800 text-gray-100 focus:outline-none focus:ring-2 focus:ring-blue-500 mb-12"
+            rows={10}
+            placeholder="내용을 입력하세요"
+            value={content}
+            onChange={(e) => setContent(e.target.value)}
+          />
 
-        {/* 버튼 */}
-        <div className="flex justify-between">
-          <button
-            type="button"
-            className="px-4 py-2 bg-blue-600 text-white rounded hover:bg-blue-700 focus:outline-none"
-            onClick={handleClickAddPostButton}
-          >
-            작성하기
-          </button>
-          <button
-            type="button"
-            className="px-4 py-2 bg-gray-700 text-white rounded hover:bg-gray-600 focus:outline-none"
-            onClick={() => alert("취소되었습니다.")}
-          >
-            취소하기
-          </button>
-        </div>
-      </form>
-    </div>
+          {/* 버튼 */}
+          <div className="flex justify-between">
+            <button
+              type="button"
+              className="px-4 py-2 bg-blue-600 text-white rounded hover:bg-blue-700 focus:outline-none"
+              onClick={handleClickAddPostButton}
+            >
+              작성하기
+            </button>
+            <button
+              type="button"
+              className="px-4 py-2 bg-gray-700 text-white rounded hover:bg-gray-600 focus:outline-none"
+              onClick={() => router.push("/")}
+            >
+              취소하기
+            </button>
+          </div>
+        </form>
+      </div>
+    </Page>
   );
 };
 
--- app/(providers)/(root)/community/posts/_components/ViewCommunityPosts.tsx
+++ app/(providers)/(root)/community/posts/_components/ViewCommunityPosts.tsx
@@ -3,14 +3,26 @@
 import unifiedAPI from "@/api/unifiedAPI";
 import { useMutation, useQuery, useQueryClient } from "@tanstack/react-query";
 import Link from "next/link";
+import { useRouter } from "next/navigation";
 import { useState } from "react";
 import Page from "../../../_components/Page";
 
 function ViewCommunityPosts() {
   const queryClient = useQueryClient();
+  const router = useRouter();
 
   const [typeBoolean, setTypeBoolean] = useState(false);
   const [typeColumn, setTypeColumn] = useState("createdAt");
+
+  const { data: userId } = useQuery({
+    queryKey: ["user"],
+    queryFn: async () => {
+      const user = await unifiedAPI.getUserApi.getUser();
+      const userId = user?.id;
+
+      return userId;
+    },
+  });
 
   const { data: getPosts } = useQuery({
     queryKey: ["posts"],
@@ -51,19 +63,21 @@ function ViewCommunityPosts() {
 
   return (
     <Page title="Posts">
-      <div className="bg-gray-900 text-gray-200 p-6 rounded-lg mr-24">
+      <div className="bg-gray-900 text-gray-200 p-6 rounded-lg mr-24 h-[550px]">
         <div className="flex gap-3 justify-end mb-4">
           {/* 정렬 옵션 */}
           <p onClick={() => handleClickSelectButton(true, "createdAt")}>OLD</p>
           <p onClick={() => handleClickSelectButton(false, "createdAt")}>NEW</p>
           <p onClick={() => handleClickSelectButton(false, "good")}>TOP</p>
         </div>
-        <ul className="space-y-4">
+
+        {/* 스크롤이 가능한 리스트 */}
+        <ul className="space-y-4 max-h-[450px] overflow-y-auto">
           {/* 리스트 출력 */}
           {getPosts?.map((posts) => (
             <li
               key={posts.postId}
-              className="bg-gray-800 p-4 rounded-lg hover:bg-gray-700 transition-colors"
+              className="bg-gray-800 p-4 rounded-lg hover:bg-gray-700 transition-colors flex items-center justify-between"
             >
               <Link href={`/community/post/${posts?.postId}`}>
                 <p className="text-xl font-semibold text-white">
@@ -71,6 +85,21 @@ function ViewCommunityPosts() {
                 </p>
                 <p className="text-base text-gray-400">{posts.content}</p>
               </Link>
+
+              <div className="text-center">
+                <p className="mb-3">글 작성자: {posts.userName}</p>
+                {/* 작성한 사람만 수정가능, 버튼 클릭시 수정 페이지로 이동 */}
+                {userId === posts.userId ? (
+                  <button
+                    className="bg-blue-950 w-[100px] h-[30px] rounded-lg"
+                    onClick={() =>
+                      router.push(`/community/post/edit/${posts.postId}`)
+                    }
+                  >
+                    수정
+                  </button>
+                ) : null}
+              </div>
             </li>
           ))}
         </ul>
