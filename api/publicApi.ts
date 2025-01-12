dc5afb02366b9b166807a45aeead621adb519d36
Mye247 <louemc1@gmail.com>
2025-01-13 03:47:13 +0900

    feat: publicApi 날씨정보 받아오기 성공

--- api/publicApi.ts
+++ api/publicApi.ts
@@ -1,20 +1,48 @@
 import axios from "axios";
+import { xml2json } from "xml-js";
 
-const getMarineStatYearbook = async () => {
+const serviceKey =
+  "38g8A68UQszljdCkecWs4YKHBtb5rI4CNORXbPT/HkZ9iisF0lK0BiY9XxC3xc1cGwtMvD4n218Q7YuUffWg0w==";
+
+const today = new Date();
+
+const base_date = today.toISOString().split("T")[0].replace(/-/g, "");
+
+const base_time =
+  today.getHours().toString().padStart(2, "0") +
+  today.getMinutes().toString().padStart(2, "0");
+
+const getUltraSrtNcst = async () => {
   try {
     const response = await axios.get(
-      "https://www.data.go.kr/data/3044767/openapi.do"
+      "http://apis.data.go.kr/1360000/VilageFcstInfoService_2.0/getUltraSrtNcst",
+      {
+        params: {
+          serviceKey: serviceKey,
+          numOfRows: 10,
+          pageNo: 1,
+          base_date: "20250113",
+          base_time: base_time,
+          nx: 55,
+          ny: 127,
+          format: "xml",
+        },
+      }
     );
 
-    return response.data;
+    const result = response.data;
+
+    // xml-js 라이브러리 사용해 json으로 변환
+    const json = xml2json(result, { compact: true, spaces: 4 });
+    return JSON.parse(json);
   } catch (error) {
-    console.error(error);
+    console.error("API 호출 중 오류 발생");
     throw error;
   }
 };
 
 const publicApi = {
-  getMarineStatYearbook,
+  getUltraSrtNcst,
 };
 
 export default publicApi;
